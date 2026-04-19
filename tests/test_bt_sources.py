dbcc070b5cf4cffc8a268d0e8b00d6461b14482d
milikii <sinzhishuibie@gmail.com>
2026-04-19 22:27:20 +0800

    test: prove bt search sort pagination flow

--- tests/test_bt_sources.py
+++ tests/test_bt_sources.py
@@ -151,6 +151,7 @@ def test_is_supported_web_source_page_url_accepts_nyaa_user_search_list_home_pag
     assert is_supported_web_source_page_url("https://nyaa.si/?f=0&c=1_2&u=subsplease")
     assert is_supported_web_source_page_url("https://nyaa.si/?f=0&c=1_2&q=frieren&p=2")
     assert is_supported_web_source_page_url("https://nyaa.si/?f=0&c=1_2&q=frieren&s=seeders&o=desc")
+    assert is_supported_web_source_page_url("https://nyaa.si/?f=0&c=1_2&q=frieren&s=seeders&o=desc&p=2")
     assert is_supported_web_source_page_url("https://nyaa.si/?f=0&c=1_2&p=2")
     assert is_supported_web_source_page_url("https://nyaa.si/?p=2")
     assert is_supported_web_source_page_url("https://nyaa.si/?s=seeders&o=desc")
@@ -184,8 +185,13 @@ def test_resolve_supported_web_source_page_request_appends_page_number() -> None
         resolve_supported_web_source_page_request("https://nyaa.si/?f=0&c=1_2&u=subsplease&s=seeders&o=desc p=2")
         == "https://nyaa.si/?f=0&c=1_2&u=subsplease&s=seeders&o=desc&p=2"
     )
+    assert (
+        resolve_supported_web_source_page_request("https://nyaa.si/?f=0&c=1_2&q=frieren&s=seeders&o=desc p=2")
+        == "https://nyaa.si/?f=0&c=1_2&q=frieren&s=seeders&o=desc&p=2"
+    )
     assert resolve_supported_web_source_page_request("https://nyaa.si/?c=1_2&s=seeders p=2") is None
     assert resolve_supported_web_source_page_request("https://nyaa.si/?f=0&c=1_2&u=subsplease&s=seeders p=2") is None
+    assert resolve_supported_web_source_page_request("https://nyaa.si/?f=0&c=1_2&q=frieren&s=seeders p=2") is None
     assert resolve_supported_web_source_page_request("https://example.com/list/42 p=2") is None
 
 
